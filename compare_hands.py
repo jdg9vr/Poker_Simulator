3b608065a0d6e36e360bde3289983e2c61b9c7c0
Joshua Gen <jdg9vr@virginia.edu>
2023-03-27 16:40:54 -0400

    Created compare_hands.py

--- compare_hands.py
+++ compare_hands.py
@@ -1,10 +1,16 @@
-num_map = {'02':'2', '03':'3', '04':'4', '05':'5', '06':'6', '07':'7', 
-           '08':'8', '09':'9', '10':'10', '11':'Jack', '12':'Queen', '13':'King', '14':'Ace'}
+from one_hand import hand_detector
 
-suit_map = {'c':'Clubs', 's':'Spades', 'd':'Diamonds', 'h':'Hearts'}
+def compare_hands(list_of_hands, community_cards):
+    full_hands = []
+    for hand in list_of_hands:
+        full_hands.append(hand+community_cards)
+    hand_rank = []
+    cards_in_order = []
+    for hand in full_hands:
+        hand_outcome = hand_detector(hand)
+        hand_rank.append(hand_outcome[0])
+        cards_in_order.append(hand_outcome[1])
+    return hand_rank.index(max(hand_rank)) # this does not handle ties
+    
 
-hand_map = {1:'Straight Flush', 2:'Four of a Kind', 3:'Full House', 
-            4:'Flush', 5:'Straight', 6:'Three of a Kind', 7:'Two Pair', 8:'Pair', 9:'High Card'}
-
-all_numbers = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14']
-all_suits = ['c', 's', 'd', 'h']
+compare_hands([['11s', '10s'], ['14c', '13d']], ['09d', '08s', '02d', '14h', '12h'])
